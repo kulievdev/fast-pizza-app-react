d88a910ec882da4dca8265c401ae799f2efbff0f
Tohir Kuliev <kuliev.tech@gmail.com>
2024-01-31 19:45:59 -0500

    played around with tailwing config file, added custom css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {}
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
     },
-    plugins: []
+    // extending colors, font-size and height to add custom css
+    extend: {
+      colors: {
+        pizza: '#12345',
+      },
+      fontSize: {
+        huge: ['80rem', { lineHeight: '1' }],
+      },
+      // over wrote screen css of tailwind
+      height: {
+        screen: '100dvh',
+      },
+    },
+  },
+  plugins: [],
 };
